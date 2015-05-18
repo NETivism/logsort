05136dc5d1144e87f3c68333035c5adecaf873ff
vps3 <vps3@netivism.com.tw>
2015-05-18 13:06:53 +0800

    Add head, tail, and specify log function.

--- apache-sort.php
+++ apache-sort.php
@@ -2,11 +2,21 @@
 <?php
 require_once('./config.inc');
 if(empty($args['type']) || empty($field[$args['type']]) ){
-  echo "  Usage:\n    --type=[ip|referer|vhost|path] --vhost=[yourdomain] --tail=20\n    --type=[ip|referer|vhost|path] --tail=20\n";
+  echo "  Usage:
+    --type=[ip|referer|vhost|path] --vhost=[yourdomain] --tail=20
+    --type=[ip|referer|vhost|path] --log=/var/log/nginx/access.log.1
+    --type=[ip|referer|vhost|path] --tail=20
+    --type=[ip|referer|vhost|path] --head=20
+";
   exit;
 }
 
 if(isset($field[$args['type']])){
+  $apachelog = $args['log'] ? $args['log'] : $apachelog;
+  $fqdnip = `hostname -i`;
+  $fqdnip = trim($fqdnip);
+  $localip = '127.0.0.1';
+
   $awk = $field[$args['type']];
   if($args['type'] == 'referer'){
     $awk = '-F\" '."'{print $".$awk."}'";
@@ -15,11 +25,25 @@ if(isset($field[$args['type']])){
     $awk = "'{print $".$awk."}'";
   }
   if(!empty($args['vhost'])){
-    $cmd = "cat {$apachelog} | grep '^{$args['vhost']}' | awk {$awk} | sort | uniq -c | sort -n | tail -{$tail}";
+    $cmd = " | grep '^{$args['vhost']}' | awk {$awk} | sort | uniq -c | sort -n | tail -20";
   }
   else{
-    $cmd = "cat {$apachelog} | awk {$awk} | sort | uniq -c | sort -n | tail -{$tail}";
+    $cmd = " | awk {$awk} | sort | uniq -c | sort -n | tail -20";
   }
+  $cmd = " | grep -v '{$fqdnip}\|127.0.0.1'".$cmd;
+
+  if($args['tail']){
+    $cmd = "tail -{$args['tail']} ${apachelog}".$cmd;
+  }
+  elseif($args['head']){
+    $cmd = "head -n {$args['head']} ${apachelog}".$cmd;
+  }
+  else{
+    $cmd = "cat ${apachelog}".$cmd;
+  }
+
+  echo "Executing...:\n".$cmd."\n";
+
   // echo $cmd;
   echo `{$cmd}`;
 }
